7d3595b4f5c1acee3ba9b814653bd027a8162d93
ruteee <rutesabreu@gmail.com>
2017-09-18 23:05:38 -0300

    correçao da classe PWM

--- PWM/PWM.cpp
+++ PWM/PWM.cpp
@@ -1,107 +1,110 @@
 #include "PWM.h"
 
-PWM:PWM(pwmName pwmName)
+PWM::PWM(pwmName pinName)
 {
-	this->pwmName = pwmName;
+	this->pwmPin = pinName;
 	loadPWM();
-	setPwmBoard()
+	setPwmBoard();
 }
 
 void PWM::loadPWM()
 {
 	system("sudo sh -c \"echo 'cape-universaln' > /sys/devices/platform/bone_capemgr/slots\"");
-	system("sudo sh -c \"echo 0 > " + PWM0_DIR;
-	system("sudo sh -c \"echo 1 > " + PWM0_DIR); 
+	
+	system(("sudo sh -c \"echo 0 > " + PWM0_DIR).c_str());
+	system(("sudo sh -c \"echo 1 > " + PWM0_DIR).c_str()); 
 
-	system("sudo sh -c \"echo 0 > " + PWM1_DIR); 
-	system("sudo sh -c \"echo 1 > " + PWM1_DIR );
+	system(("sudo sh -c \"echo 0 > " + PWM1_DIR).c_str()); 
+	system(("sudo sh -c \"echo 1 > " + PWM1_DIR).c_str());
 
-	system("sudo sh -c \"echo 0 > " + PWM2_DIR);
-	system("sudo sh -c \"echo 1 > " + PWM2_DIR); 
+	system(("sudo sh -c \"echo 0 > " + PWM2_DIR).c_str());
+	system(("sudo sh -c \"echo 1 > " + PWM2_DIR).c_str()); 
 }
 
 
 void PWM::setPwmBoard(){
-	switch (this->pwmName){
+	switch (this->pwmPin){
 		case 0:
-			this.baseDIR = PWM2_DIR + "/pwm1";
+			this->baseDir = PWM2_DIR + "/pwm1";
 			break;
 		case 1:
-			this.baseDIR = PWM2_DIR + "/pwm0";
+			this->baseDir = PWM2_DIR + "/pwm0";
 			break;
 		case 2:
-			this.baseDIR = PWM1_DIR + "/pwm0";
+			this->baseDir = PWM1_DIR + "/pwm0";
 			break;
 		case 3:
-			this.baseDIR = PWM1_DIR + "/pwm1";
+			this->baseDir = PWM1_DIR + "/pwm1";
 			break;
 		case 4:
-			this.baseDIR = PWM0_DIR + "/pwm1";
+			this->baseDir = PWM0_DIR + "/pwm1";
 			break;
 		case 5:
-			this.baseDIR = PWM0_DIR + "/pwm0";
+			this->baseDir = PWM0_DIR + "/pwm0";
 			break;
 		default:
-			std::cout << "Pino invalido"
+			std::cout << "Pino invalido";
 		
 	}
+	
 
 }
 
 
 void PWM::setPeriod(int period)
 {
-	fstream fs;
-	fs.open(this->baseDIR + "/period", fstream::out);
-	fs << period
+	std::fstream fs;
+	fs.open(this->baseDir + "/period", fstream::out);
+	fs << period;
 	fs.close();
 }
 
 int PWM::getPeriod()
 {
 	float period;
-	fstream fs;
-	fs.open(this->baseDir.c_str(), fstream::in);
-	fs >> retorno;
+	std::fstream fs;
+	fs.open(this->baseDir + "/period", fstream::in);
+	fs >> period;
 	fs.close();
-	return retorno;
+	return period;
 }
 
 void PWM::setDutyCycle(int duty)
 {
-	fstream fs;
-	fs.open(this->baseDIR + "/duty_cycle", fstream::out);
-	fs << duty
+	std::fstream fs;
+	fs.open(this->baseDir + "/duty_cycle", fstream::out);
+	fs << duty;
 	fs.close();
 }
 
 int PWM::getDutyCycle()
 {
 	int duty;
-	fstream fs;
-	fs.open(this->baseDir.c_str(), fstream::in);
+	std::fstream fs;
+	fs.open(this->baseDir + "/duty_cycle", fstream::in);
 	fs >> duty;
 	fs.close();
-	return retorno;
+	return duty;
 }
 
 void PWM::setState(statePwm state)
 {
 	if (state == 0 || state == 1){
-		fstream fs;
-		fs.open(this->baseDIR + "/enable", fstream::out);
-		fs << state
+		std::fstream fs;
+		fs.open(this->baseDir + "/enable", fstream::out);
+		fs << state;
 		fs.close();
+		std::cout << this->baseDir + "/enable";
 	}
 }
 
 int PWM::getState()
 {
 	int state;
-	fstream fs;
-	fs.open(this->baseDir.c_str(), fstream::in);
+	std::fstream fs;
+	fs.open(this->baseDir + "/enable", fstream::in);
 	fs >> state;
 	fs.close();
-	return retorno;
+	return state;
 }
 
